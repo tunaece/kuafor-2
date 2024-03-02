4c750993dc0a0b6e056ad985f21517267b566cd8
Tuna <tunaece063@gmail.com>
2024-03-02 20:38:07 +0300

    classes

--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -12,40 +12,40 @@ const ServiceCards = () => {
       {
          title: 'SAÇ İŞLEMLERİ',
          link: '/sac-islemleri',
-         image: '/public/assets/dark-hair.jpg'
+         clas: 'sac'
       },
       {
          title:'MAKYAJ İŞLEMLERİ',
          link:'/makyaj-islemleri',
-         image:'/public/assets/makyaj.jpg'
+         clas:'makyaj'
       },
       {
          title:'GELİN SAÇI VE MAKYAJI',
          link:'/gelin-saci-ve-makyajı',
-         image:'/public/assets/bride-hair.jpg'
+         clas:'gelin'
       },
       {
          title:'EL & AYAK İŞLEMLERİ',
          link:'/el-ayak-islemleri',
-         image:'/public/assets/protez-tirnak.jpg'
+         clas:'elayak'
       },
       {
          title:'MİKRO KAYNAK',
          link:'/mikro-kaynak',
-         image:'/public/assets/mikro.jpg'
+         clas:'mikro'
       },
       {
          title:'AGDA İŞLEMLERİ',
          link:'/agda-islemleri',
-         image:'/public/assets/agda.jpg'
+         clas:'agda'
       }
    ]
 return (
 <div className="serv-wrapper">
 {
    services.map((serv,ix)=>(
-      <div style={{backgroundImage:`url(${serv.image})`,backgroundPosition:'center'}}
-       className="servCard"  key={ix}
+      <div 
+       className={serv.clas} id="servCard"  key={ix}
       onClick={()=> detayli(serv.link)}>
          <h2 className="servTitle"> {serv.title} </h2>
          <button onClick={()=>detayli(serv.link)}
