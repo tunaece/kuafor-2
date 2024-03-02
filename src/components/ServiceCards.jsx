2fe691ec7795813838284d69823cdb15c62563ec
Tuna <tunaece063@gmail.com>
2024-03-02 20:20:35 +0300

    eee

--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -12,39 +12,39 @@ const ServiceCards = () => {
       {
          title: 'SAÇ İŞLEMLERİ',
          link: '/sac-islemleri',
-         image: 'public/assets/dark-hair.jpg'
+         image: '/public/assets/dark-hair.jpg'
       },
       {
          title:'MAKYAJ İŞLEMLERİ',
          link:'/makyaj-islemleri',
-         image:'public/assets/makyaj.jpg'
+         image:'/public/assets/makyaj.jpg'
       },
       {
          title:'GELİN SAÇI VE MAKYAJI',
          link:'/gelin-saci-ve-makyajı',
-         image:'public/assets/bride-hair.jpg'
+         image:'/public/assets/bride-hair.jpg'
       },
       {
          title:'EL & AYAK İŞLEMLERİ',
          link:'/el-ayak-islemleri',
-         image:'public/assets/protez-tirnak.jpg'
+         image:'/public/assets/protez-tirnak.jpg'
       },
       {
          title:'MİKRO KAYNAK',
          link:'/mikro-kaynak',
-         image:'public/assets/mikro.jpg'
+         image:'/public/assets/mikro.jpg'
       },
       {
          title:'AGDA İŞLEMLERİ',
          link:'/agda-islemleri',
-         image:'public/assets/agda.jpg'
+         image:'/public/assets/agda.jpg'
       }
    ]
 return (
 <div className="serv-wrapper">
 {
    services.map((serv,ix)=>(
-      <div style={{backgroundImage:`url(/${serv.image})`,backgroundPosition:'center'}}
+      <div style={{backgroundImage:`url(${serv.image})`,backgroundPosition:'center'}}
        className="servCard"  key={ix}
       onClick={()=> detayli(serv.link)}>
          <h2 className="servTitle"> {serv.title} </h2>
